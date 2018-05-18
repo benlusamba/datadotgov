ba4a1af8bdb2720f96f74bdfca3fc177b7b1b663
george <benlusamba@gmail.com>
2018-05-18 15:58:45 -0400

    Gender

---
+++ jobs_male.py
@@ -0,0 +1,23 @@
+#Male unemployment
+
+import requests
+import json
+import prettytable
+headers = {'Content-type': 'application/json'}
+data = json.dumps({"seriesid": ['LNS14000025'],"startyear":"2009", "endyear":"2018"})
+p = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
+json_data = json.loads(p.text)
+for series in json_data['Results']['series']:
+    x=prettytable.PrettyTable(["series id","year","period","value"]) #add "footnotes"
+    seriesId = series['seriesID']
+    for item in series['data']:
+        year = item['year']
+        period = item['period']
+        value = item['value']
+
+        x.add_row([seriesId,year,period,value])
+
+    output = open(seriesId + '.txt','w') #export as .csv or .txt file
+    output.write (x.get_string())
+    output.close()
+    print(x)
